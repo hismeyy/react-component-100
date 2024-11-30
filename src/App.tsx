c4c6b9d1c1cc057647f040a38e2a3112989644b4
MaxCosmos <renyh2001@outlook.com>
2024-11-30 17:55:50 +0800

    挑战用React封装100个组件【004】

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css'
-import InfoCard from './components/card/infoCard03/InfoCard'
+import Img from './components/img/img01/Img'
 
 function App() {
+  const handleImageClose = () => {
+    console.log('图片被关闭了');
+    // 这里可以执行任何你需要的关闭逻辑
+  };
+
   return (
     <>
-      <InfoCard />
+      <Img
+        src="https://picsum.photos/200"
+        alt="Random image"
+        size="large"
+        onClose={handleImageClose}
+      />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/img/img01/Img.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './Img.css'
+import { MdClose } from 'react-icons/md';
+
+interface ImgProps {
+    src: string;
+    alt: string;
+    size?: 'small' | 'medium' | 'large';
+    onClose?: () => void;
+}
+
+const Img = ({ src, alt, size = 'small', onClose }: ImgProps) => {
+    const [showPreview, setShowPreview] = useState(false);
+    const [visible, setVisible] = useState(true);
+
+    const handleClose = () => {
+        setVisible(false);
+        if (onClose) {
+            onClose();
+        }
+    };
+
+    if (!visible) return null;
+
+    return (
+        <div className='img-container'>
+            <button className={`img-close img-close-${size}`} onClick={handleClose}><MdClose /></button>
+            <div className={`img img-${size}`} onClick={() => setShowPreview(true)}>
+                <img src={src} alt={alt} />
+            </div>
+
+            {showPreview && (
+                <div className="img-preview-overlay" onClick={() => setShowPreview(false)}>
+                    <div className="img-preview-content">
+                        <img src={src} alt={alt} />
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Img
